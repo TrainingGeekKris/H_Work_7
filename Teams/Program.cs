93b90377da0035df81d49da8453930f5f2d005a1
Kristina <petrovnina22@mail.ru>
2023-01-16 19:58:23 +0300

    team_52

--- Teams/Program.cs
+++ Teams/Program.cs
@@ -31,12 +31,45 @@
 //8 4 2 4
 //17 -> такого числа в массиве нет
 
-Console.WriteLine("Введите номер строки");
+//Console.WriteLine("Введите номер строки");
+//int rows= Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите номер столбца");
+//int columns= Convert.ToInt32(Console.ReadLine());
+
+//int [,] matrix = new int[5,5];
+
+//for (int i = 0; i < matrix.GetLength(0); i++)
+//{
+//    for (int j = 0; j < matrix.GetLength(1); j++)
+//    {
+//        matrix[i,j]= new Random().Next(10);
+//        Console.Write(matrix[i,j]+" ");
+//    }
+//     Console.WriteLine();
+//}
+//if (rows<=matrix.GetLength(0)&&columns<=matrix.GetLength(1))
+//{
+//    Console.Write(matrix[rows-1,columns-1]);
+//}
+//else
+//{
+//    Console.Write("Такого числа в массиве нет");
+//}
+
+// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Введите количество строк");
 int rows= Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите номер столбца");
+Console.WriteLine("Введите количество столбцов");
 int columns= Convert.ToInt32(Console.ReadLine());
 
-int [,] matrix = new int[5,5];
+int [,] matrix = new int[rows,columns];
 
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
@@ -45,13 +78,17 @@ for (int i = 0; i < matrix.GetLength(0); i++)
         matrix[i,j]= new Random().Next(10);
         Console.Write(matrix[i,j]+" ");
     }
-     Console.WriteLine();
+    Console.WriteLine();
 }
-if (rows<=matrix.GetLength(0)&&columns<=matrix.GetLength(1))
+Console.WriteLine();
+for (int j = 0; j < matrix.GetLength(1); j++)
 {
-    Console.Write(matrix[rows-1,columns-1]);
-}
-else
-{
-    Console.Write("Такого числа в массиве нет");
+    double avarage = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        avarage = (avarage + matrix[i, j]);
+    }
+    avarage = avarage / rows;
+    
+    Console.Write(avarage + "; ");
 }
